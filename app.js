eb51c962e8a5041d344f34997faf718c6756e0b8
Luis Alves <lg.alvesz11@gmail.com>
2025-03-31 18:30:11 -0300

    Implementação do front-end

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const clientesRouter = require('./routes/clientes');
 const produtosRouter = require('./routes/produtos');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use('/clientes', clientesRouter);
 app.use('/produtos', produtosRouter);
 
-// Rota padrão
-app.get('/', (req, res) => {
-  res.send('API do Desafio Express + MySQL');
-});
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'index.html')); });
 
 module.exports = app;
